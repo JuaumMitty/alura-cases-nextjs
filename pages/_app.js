7885fb85a10eefdbf48621d26b4b0cb52b73a926
JuaumMitty <jjoaovictorpg@gmail.com>
2022-02-28 11:20:08 -0300

    Finalizando aula 4

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,29 @@
 import GlobalStyle from "../src/theme/GlobalStyle";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
+
+                {/* <!-- Google Tag Manager --> */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-XR86J93SQW"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                      
+                        gtag('config', 'G-XR86J93SQW');
+                        `
+                    }}
+                />
+
+            </Head>
             <GlobalStyle />
             <Component {...pageProps} />
         </>
--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,47 @@
+import { faC } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from 'react';
 import Link from "../src/components/Link";
+import Head from "next/head";
+import PageTitle from "../src/components/PageTitle";
+
+export async function getStaticProps() {
+    return {
+        props: {
+            qualquercoisa: "que eu passar aqui"
+        }
+    }
+}
 
 export default function FAQPage() {
+    const [faq, setFaq] = useState([]);
+    useEffect(() => {
+        const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+
+        fetch(FAQ_API_URL)
+            .then((respostaDoServidor) => {
+                return respostaDoServidor.json();
+            })
+            .then((resposta) => {
+                setFaq(resposta);
+            })
+    }, []);
     return (
         <div>
+            <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
             <h1>Alura Cases - Página de perguntas FAQ</h1>
             <Link href="/">
                 Ir para a página Home
             </Link>
+            <ul>
+                {faq.map(({ answer, question }) => (
+                    <li key={question}>
+                        <article >
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
